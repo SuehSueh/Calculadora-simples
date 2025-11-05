cbcf43255265e038477d8f029db03fbc11657f54
Matheus <matheuskalisto2016@gmail.com>
2025-11-05 19:35:48 -0300

    função resultado adicionada

--- Calculadorajavascript.js
+++ Calculadorajavascript.js
@@ -3,7 +3,29 @@ function insert(N)
 var numero = document.getElementById('resultado').innerHTML;
 document.getElementById('resultado').innerHTML = numero + N;
 }
+
+
 function clean()
 {
     document.getElementById('resultado').innerHTML = "";
+}
+
+
+function back()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
+}
+function calcular()
+
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado)
+    {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else
+    {
+        document.getElementById('resultado').innerHTML = "Insira um valor"
+    }
 }
