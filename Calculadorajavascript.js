4e33f13089de6266684b108bb5ca26d0ffc4ce52
Matheus <matheuskalisto2016@gmail.com>
2025-11-26 19:41:36 -0300

    adicionado nova função historico, javascript detalhado

--- Calculadorajavascript.js
+++ Calculadorajavascript.js
@@ -1,31 +1,71 @@
-function insert(N)
-{
-var numero = document.getElementById('resultado').innerHTML;
-document.getElementById('resultado').innerHTML = numero + N;
+
+/*inserir número*/
+function insert(N) {
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + N;
 }
 
 
-function clean()
-{
+/* apagar tudo*/
+function clean() {
     document.getElementById('resultado').innerHTML = "";
 }
 
 
-function back()
-{
+/*Apagar 1 digito*/
+function back() {
     var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
 }
-function calcular()
+/*array do historico*/
+let historico = [];
 
-{
-    var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado)
-    {
-        document.getElementById('resultado').innerHTML = eval(resultado);
-    }
-    else
-    {
-        document.getElementById('resultado').innerHTML = "Insira um valor"
+/*execução de calculo*/
+function calcular() {
+    var valor = document.getElementById('resultado').innerHTML;
+    if (valor) {
+        try {
+            let resultado = eval(valor);
+            document.getElementById('resultado').innerHTML = resultado;
+            adicionarHistorico(valor + " = " + resultado);
+            
+        } catch {
+            document.getElementById('resultado').innerHTML = "Erro";
+        }
     }
 }
+
+/* SALVAR NO HISTÓRICO */
+function adicionarHistorico(item) {
+    historico.unshift(item);
+    if (historico.length > 12) historico.pop();
+    atualizarHistorico();
+}
+
+function atualizarHistorico() {
+    let lista = document.getElementById('historico');
+    lista.innerHTML = "";
+
+    historico.forEach(item => {
+        let li = document.createElement("li");
+        li.textContent = item;
+        lista.appendChild(li);
+    });
+}
+
+/* CONTROLE DO SLIDE */
+const container = document.getElementById("historicoContainer");
+const seta = document.getElementById("toggleSeta");
+let aberto = false;
+
+seta.addEventListener("click", () => {
+    aberto = !aberto;
+
+    if (aberto) {
+        container.classList.add("mostrar");
+        seta.classList.add("ativa");
+    } else {
+        container.classList.remove("mostrar");
+        seta.classList.remove("ativa");
+    }
+});
